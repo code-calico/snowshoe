62d9f8568d5a63480d437cf03798f588bb0a6bb6
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2024-04-10 18:02:00 -0700

    title

---
+++ title_screen.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+public partial class title_screen : Node
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+
+	public void basebutton.button.up();
+}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}

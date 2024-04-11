4e8dfb9ce8f61049daf626002d6e167f33403dd3
Isabelle Hall <121070808+nik0-dev@users.noreply.github.com>
2024-04-10 20:37:13 -0500

    title button event

--- title_screen.cs
+++ title_screen.cs
@@ -1,15 +1,17 @@
 using Godot;
 using System;
-public partial class title_screen : Node
+public partial class title_screen : Button
 {
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
+	public override void _Ready() {
+		this.ButtonUp += OnButtonUp;
+	}
 
-}
+	private void OnButtonUp() {
+		GD.Print("Please fucking work!");
+	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+	public override void _Process(double delta) {
 	}
 }

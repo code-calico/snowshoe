9fc0378221bdb777a97cae8da110cb06b0714bc4
Isabelle Hall <121070808+nik0-dev@users.noreply.github.com>
2024-04-12 03:50:57 -0500

    .tscn file param export
    used to do a directory search by name, now we can just specify the .tscn file directly

--- src/utils/SceneButton.cs
+++ src/utils/SceneButton.cs
@@ -4,59 +4,19 @@ using System.Diagnostics;
 using System.IO;
 
 public partial class SceneButton : Button
-{
-	// these exports are meant to be adjusted in the inspector please don't touch here! 
-	[Export] public string folderName = "";
-	[Export] public string sceneName = "";
-	[Export] public bool debug = false;
+{	
+
+	// set the scene variable in the inspector rather than in code please !
+	[Export(PropertyHint.File, "*.tscn")] 
+	public string scene = "";
 
 	public override void _Ready() => this.ButtonUp += OnButtonUp;
 
 	// "button_up" signal subscription
 	private void OnButtonUp() {
 
-		string workingDir = ProjectSettings.GlobalizePath($"res://scene/{folderName}");
-		string finalPath = $"{workingDir}/{sceneName}.tscn";
-
-		if (Directory.Exists(workingDir)) {
-			SearchForScene(workingDir, finalPath);
-		} else {
-			GD.PrintErr($"Directory does not exist: '{workingDir}' ...");
-		}
-	}
-
-	private void SearchForScene(string searchDir, string filePath) {
-		string[] paths = Directory.GetFiles(searchDir);
-		
-		bool success = false;
-		
-		foreach(string path in paths) {
-			DebugMessage($"Matching built path against '{path}' ...");
-			
-			if (File.Exists(filePath)) {
-				DebugMessage($"Found '{filePath}'! ...");
-				LoadScene(filePath);
-				success = true;
-				break;
-			} else {
-				DebugMessage("Match failure ...");
-			}
-		}
-		
-		if (!success) {
-			GD.PrintErr($"File does not exist: '{filePath}' ...");
-		} 
-	}
-
-	private void LoadScene(string scenePath) {
-		PackedScene sceneResource = (PackedScene)ResourceLoader.Load(scenePath);
+		string pathToScene = ProjectSettings.GlobalizePath(scene);
+		PackedScene sceneResource = (PackedScene)ResourceLoader.Load(pathToScene);
 		GetTree().ChangeSceneToPacked(sceneResource);
-	}
-
-
-	private void DebugMessage(string message) {
-		if(debug) {
-			GD.Print(message);
-		}
 	}
 }

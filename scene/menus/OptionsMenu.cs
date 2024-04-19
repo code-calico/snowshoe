6e5e8896f6b93f5a733ca6ad704359c8d943eb66
nik0-dev <121070808+nik0-dev@users.noreply.github.com>
2024-04-19 17:48:29 -0500

    added options panels

--- scene/menus/OptionsMenu.cs
+++ scene/menus/OptionsMenu.cs
@@ -1,19 +1,39 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class OptionsMenu : Control
 {
 	[ExportGroup("Back Button")]
 	[Export] Button back;
 	[Export(PropertyHint.File, ".tscn")] string mainMenuPath;
+	
+	[Export] TabBar bar;
+	[Export] Panel[] panels;
+	
 
 	public override void _Process(double delta) {
 		if (Input.IsActionJustPressed("ui_cancel")) { Back(); }
 	}
 
 	public override void _Ready() {
+		for (int i = 0; i < panels.Length; i++) {
+			panels[i].Visible = false;
+		}
+		panels[0].Visible = true;
+		
 		back.ButtonUp += Back;
+		bar.TabChanged += TabSelected;
+	}
+
+	void TabSelected(long tab) {
+		for (int i = 0; i < panels.Length; i++) {
+			panels[i].Visible = false;
+		}
+		panels[tab].Visible = true;
 	}
 
 	void Back() => SceneManager.Load(mainMenuPath);
 }
+
+

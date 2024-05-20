169cadf5ba6cb0ac679b532374a3d3b73f3444ea
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2024-05-20 01:06:02 -0700

    added hacky support for writing settings to config file. reading doesnt work rn but im tired

--- src/menu/VideoSettings.cs
+++ src/menu/VideoSettings.cs
@@ -21,20 +21,32 @@ public partial class VideoSettings : Panel
 		InitSubscriptions();
 	}
 	
-	void InitSubscriptions() {		
-		resolutionSelector.ItemSelected += (long idx) => { 
+	void InitSubscriptions() {
+
+	var config = new ConfigFile();
+		resolutionSelector.ItemSelected += (long idx) => {
 			Vector2I resolution = ResolutionOptions.GetArray()[(int)idx];
-			DisplayServer.WindowSetSize(resolution); 
+			DisplayServer.WindowSetSize(resolution);
+			config.SetValue("video", "resolution", resolution);
+		config.Save("user://video.cfg");
 		};
 
 		fullscreenSelector.ItemSelected += (long idx) => { 
 			DisplayServer.WindowMode fullscreenMode = FullscreenOptions.GetArray()[(int)idx]; 
-			DisplayServer.WindowSetMode(fullscreenMode); 
+			DisplayServer.WindowSetMode(fullscreenMode);
+			//very very hacky so sorry to whoever reads this
+		string currentFullscreenMode = Convert.ToString(fullscreenMode);
+			config.SetValue("video", "fullscreenMode", currentFullscreenMode);
+		config.Save("user://video.cfg");
 		};
 
 		vsyncSelector.ItemSelected += (long idx) => { 
 			DisplayServer.VSyncMode vsyncMode = VSyncOptions.GetArray()[(int)idx];
-			DisplayServer.WindowSetVsyncMode(vsyncMode); 
+			DisplayServer.WindowSetVsyncMode(vsyncMode);
+			string currentVsyncMode = Convert.ToString(vsyncMode);
+			config.SetValue("video", "vsyncMode", currentVsyncMode);
+			//may allah forgive me for the absolute dogshit way i've implemented this
+		config.Save("user://video.cfg");
 		};
 
 		fpsSelector.ValueChanged += (double val) => { Engine.MaxFps = (int)val; };
@@ -53,5 +65,24 @@ public partial class VideoSettings : Panel
 			btn.AddItem(values[i]);
 		}
 		btn.Select(indexFocus); // selects project default
+	}
+
+	void readConfigFile()
+	{
+		var config = new ConfigFile();
+
+// Load data from a file.
+	Error err = config.Load("user://scores.cfg");
+
+// If the file didn't load, ignore it.
+	if (err != Error.Ok) {
+    return;
+	}
+	foreach (string video in config.GetSections())
+	{
+	var test = config.GetValue(video, "resolution");
+	DisplayServer.WindowSetMode(test);
+	}
+
 	}
 }

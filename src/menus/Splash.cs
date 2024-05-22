9760e4254dcd4ae2641bf7f129d00859cd6e41b9
nik0-dev <121070808+nik0-dev@users.noreply.github.com>
2024-05-21 20:35:36 -0500

    misc tweaks and splash skip

--- src/menus/Splash.cs
+++ src/menus/Splash.cs
@@ -7,8 +7,18 @@ public partial class Splash : Control
 
 	public override void _Ready(){
 		timer = GetNode<Timer>("%SplashDuration");	
-		timer.Timeout += () => SceneManager.Load("res://scenes/menus/title.tscn");
+		timer.Timeout += SplashFinished; 
+		
 	}
 
+	public override void _UnhandledInput(InputEvent @event) {
+		if (@event.IsPressed()) {
+			GD.Print("test");
+			timer.Timeout -= SplashFinished;
+			SceneManager.Load("res://scenes/levels/dev/lvl-dev-test.tscn");
+		}
+	}
+
+	private void SplashFinished() => SceneManager.Load("res://scenes/menus/title.tscn");
 
 }

6ada788eb619c5adf64abe950b8cae385189abbe
Isabelle Hall <121070808+nik0-dev@users.noreply.github.com>
2024-05-28 09:35:07 -0500

    fixed stupid typo, works now

--- src/util/SceneFiles.cs
+++ src/util/SceneFiles.cs
@@ -6,7 +6,7 @@ public static class SceneFiles {
     public static readonly string directory = "res://scenes/";
 
     public static class Transitions {
-        public static readonly string directory = SceneFiles.directory + "transitions";
+        public static readonly string directory = SceneFiles.directory + "transitions/";
 
         public static readonly string FADE = BuildPath("fade_transition.tscn");
 
@@ -14,7 +14,7 @@ public static class SceneFiles {
     }
 
     public static class Menus {
-        public static readonly string directory = SceneFiles.directory + "menus";
+        public static readonly string directory = SceneFiles.directory + "menus/";
 
         public static readonly string MAIN = BuildPath("title.tscn");
         public static readonly string SPLASH = BuildPath("splash.tscn"); 
@@ -23,9 +23,9 @@ public static class SceneFiles {
     }
 
     public static class Levels {
-        public static readonly string directory = SceneFiles.directory;
+        public static readonly string directory = SceneFiles.directory + "levels/";
 
-        public static readonly string DEV_TESTING = BuildPath("dev/level-dev-test.tscn");
+        public static readonly string DEV_TESTING = BuildPath("dev/lvl-dev-test.tscn");
 
         public static string BuildPath(string path) => directory + path;
     }

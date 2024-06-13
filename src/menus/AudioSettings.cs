fa4e3fe83ee69e17b412ef0083fc4dd703eeb3a4
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2024-06-13 03:12:40 -0700

    scaffolding. im tired and ill try to write the actual fullon logic tommorow

--- src/menus/AudioSettings.cs
+++ src/menus/AudioSettings.cs
@@ -1,10 +1,15 @@
 using Godot;
 using System;
 
-public partial class AudioSettings : Node
+public partial class AudioSettings : Panel
 {
-	Slider VolumeSlider;
+	HSlider MasterVolumeSlider;
+	HSlider SFXVolumeSlider;
+	HSlider MusicVolumeSlider;
 
+	ConfigKey MasterVolume = ConfigKeys.Audio.MasterVolume;
+	ConfigKey SFXVolume = ConfigKeys.Audio.SFXVolume;
+	ConfigKey MusicVolume = ConfigKeys.Audio.MusicVolume;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -13,5 +18,11 @@ public partial class AudioSettings : Node
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+	void InitReferences()
+	{
+		MasterVolumeSlider = GetNode<HSlider>("%MasterVolumeSlider");
+		MasterVolumeSlider = GetNode<HSlider>("%SFXVolumeSlider");
+		MasterVolumeSlider = GetNode<HSlider>("%MusicVolumeSlider");
 	}
 }
